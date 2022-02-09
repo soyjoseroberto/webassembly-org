be27038db9723a68fa40cca453dffc22644e46d6
soyjoseroberto <jrodriguez18@gmail.com>
2022-02-08 23:33:10 -0500

    Added wasm_bindgen(start) function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen(start)]
+pub fn run() {
+    bare_bones();
+    // using_a_macro();
+    // using_web_sys();
+}
+
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+fn bare_bones() {
+    log("Hello from Rust!");
+}
+
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
@@ -8,4 +27,9 @@ extern {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn add(n1: i32, n2:i32) -> i32 {
+    n1 + n2
 }
